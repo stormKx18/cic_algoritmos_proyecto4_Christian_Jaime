4c3710c872b3485aa7dd7abb0cf427ca7b0b4519
stormKx18 <vicjaime922@gmail.com>
2021-05-20 21:24:46 -0500

    gilbert y geografico (gv)

--- src/kruskal_prim_ejemplos.py
+++ src/kruskal_prim_ejemplos.py
@@ -93,6 +93,95 @@ prim=gfErdosReny.Prim()
 prim.display()
 prim.graphviz()
 
-#******************************************************************************
 '''
+
+'''
+#******************************************************************************
+from grafoGilbert import grafoGilbert
+#******************************************************************************
+#grafoGilbert - Pocos nodos
+gfGilbert = grafoGilbert(n=20, p=0.1, dirigido=False, auto=False)
+gfGilbert.display()
+gfGilbert.graphviz()
+
+#Kruskal directo
+KruskalD=gfGilbert.KruskalD()
+KruskalD.display()
+KruskalD.graphviz()
+
+#Kruskal Inverso
+KruskalI=gfGilbert.KruskalI()
+KruskalI.display()
+KruskalI.graphviz()
+
+#Prim
+prim=gfGilbert.Prim()
+prim.display()
+prim.graphviz()
+
+
+#grafoGilbert - Muchos nodos
+gfGilbert = grafoGilbert(n=100, p=0.1, dirigido=False, auto=False)
+gfGilbert.display()
+gfGilbert.graphviz()
+
+#Kruskal directo
+KruskalD=gfGilbert.KruskalD()
+KruskalD.display()
+KruskalD.graphviz()
+
+#Kruskal Inverso
+KruskalI=gfGilbert.KruskalI()
+KruskalI.display()
+KruskalI.graphviz()
+
+#Prim
+prim=gfGilbert.Prim()
+prim.display()
+prim.graphviz()
+'''
+
+#******************************************************************************
+from grafoGeografico import grafoGeografico
+#******************************************************************************
+#grafoGeografico - Pocos nodos
+gfGeografico = grafoGeografico(n=30, r=0.3, dirigido=False, auto=False)
+gfGeografico.display()
+gfGeografico.graphviz()
+
+#Kruskal directo
+KruskalD=gfGeografico.KruskalD()
+KruskalD.display()
+KruskalD.graphviz()
+
+#Kruskal Inverso
+KruskalI=gfGeografico.KruskalI()
+KruskalI.display()
+KruskalI.graphviz()
+
+#Prim
+prim=gfGeografico.Prim()
+prim.display()
+prim.graphviz()
+
+#grafoGeografico - Muchos nodos
+gfGeografico = grafoGeografico(n=100, r=0.3, dirigido=False, auto=False)
+gfGeografico.display()
+gfGeografico.graphviz()
+
+#Kruskal directo
+KruskalD=gfGeografico.KruskalD()
+KruskalD.display()
+KruskalD.graphviz()
+
+#Kruskal Inverso
+KruskalI=gfGeografico.KruskalI()
+KruskalI.display()
+KruskalI.graphviz()
+
+#Prim
+prim=gfGeografico.Prim()
+prim.display()
+prim.graphviz()
+#******************************************************************************
 print("Execution time --- %s seconds ---" % round((time.time() - start_time),2))
